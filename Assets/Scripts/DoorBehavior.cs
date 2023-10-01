6eb4ff3e0f89071be4398810a1c58ecf9844b1bb
ChristinMatt <christin.matt@supercell.com>
2023-10-01 12:59:18 +0300

    cleanup
    fixed enemy kill particle
    made single level exit instead of tilemap

--- Assets/Scripts/DoorBehavior.cs
+++ Assets/Scripts/DoorBehavior.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class DoorBehavior : MonoBehaviour
 {
     private GameManager _gameManager;
+    [SerializeField] private ParticleSystem exitParticles;
 
     private void Start()
     {
@@ -16,6 +18,7 @@ public class DoorBehavior : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Player"))
         {
+            Instantiate(exitParticles, transform.position, quaternion.identity);
             _gameManager.OnExitReached();
         } 
     }
